77244ba772d1a4bef88d8ece36166d26da453dc1
Stefan Ziel <zis@claninfo.ch>
2016-12-16 16:50:54 -0200

    Änderung der Abbildung zwischen json und xml. Damit einhergehende Anpassung am Schema und Validator.

---
+++ src/main/java/ch/claninfo/json/JSON2DOM.java
@@ -0,0 +1,117 @@
+/* $Id$ */
+
+package ch.claninfo.json;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+/**
+ *
+ */
+public class JSON2DOM extends AbstractJSONParser {
+
+	/** Namespace */
+	public static final String NS = "http://www.claninfo.ch/2016/pkOne-Meldungen"; //$NON-NLS-1$
+	private static final String ARRAY_NAME_ATTR = "array-name"; //$NON-NLS-1$
+	private static DocumentBuilder builder;
+
+	Document doc;
+	Element curr;
+	String name;
+
+	private JSON2DOM(Reader pIn, String pName) throws IOException, ParserConfigurationException {
+		super(pIn);
+		if (builder == null) {
+			builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		}
+		doc = builder.newDocument();
+		name = pName;
+	}
+
+	/**
+	 * @param pIn input stream containing JSON Object
+	 * @param pName Name of the Message
+	 * @return Dom Document
+	 * @throws JSONParseException invalid json
+	 * @throws IOException
+	 */
+	public static Document parse(Reader pIn, String pName) throws JSONParseException, IOException {
+		try {
+			JSON2DOM p = new JSON2DOM(pIn, pName);
+			p.parse();
+			return p.doc;
+		}
+		catch (ParserConfigurationException e) {
+			throw new IOException(e);
+		}
+	}
+
+	@Override
+	protected void endArray() throws JSONParseException {
+		curr.removeAttribute(ARRAY_NAME_ATTR);
+	}
+
+	@Override
+	protected void endObject() throws JSONParseException {
+		Node parent = curr.getParentNode();
+		if (parent.getNodeType() == Node.ELEMENT_NODE) {
+			curr = (Element) parent;
+			name = curr.getAttribute(ARRAY_NAME_ATTR);
+		} else {
+			curr = null;
+		}
+	}
+
+	@Override
+	protected void name(String pName) throws JSONParseException {
+		name = pName;
+	}
+
+	/**
+	 * @see ch.claninfo.server.json.AbstractJSONParser#startArray()
+	 */
+	@Override
+	protected void startArray() throws JSONParseException {
+		curr.setAttribute(ARRAY_NAME_ATTR, name);
+	}
+
+	@Override
+	protected void startObject() throws JSONParseException {
+		Element obj = doc.createElementNS(NS, name);
+		if (curr == null) {
+			doc.appendChild(obj);
+		} else {
+			curr.appendChild(obj);
+		}
+		curr = obj;
+	}
+
+	@Override
+	protected void value(Object pObject) throws JSONParseException {
+		if (pObject != null) {
+			String value;
+			if (pObject instanceof Date) {
+				value = STD_DATE_FORMAT.format(pObject);
+			} else {
+				value = pObject.toString();
+			}
+
+			if (curr.hasAttribute(name)) {
+				curr.setAttribute(name, curr.getAttribute(name) + ' ' + value);
+			} else if ("Content".equals(name)) { //$NON-NLS-1$
+				curr.appendChild(doc.createTextNode(value));
+			} else {
+				curr.setAttribute(name, value);
+			}
+		}
+	}
+}
--- src/test/java/ch/claninfo/json/JSON2DOMTest.java
+++ src/test/java/ch/claninfo/json/JSON2DOMTest.java
@@ -7,9 +7,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import javax.xml.XMLConstants;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.ValidatorHandler;
+import javax.xml.validation.Validator;
 
 import org.junit.Test;
 import org.xml.sax.SAXException;
@@ -17,11 +18,11 @@ import org.xml.sax.SAXException;
 /**
  * 
  */
-public class JSON2SAXTest {
+public class JSON2DOMTest {
 
 	/**
 	 * Test method for
-	 * {@link ch.claninfo.json.JSON2SAX#parse(java.io.Reader, java.lang.String, org.xml.sax.ContentHandler)}.
+	 * {@link ch.claninfo.json.JSON2DOM#parse(java.io.Reader, java.lang.String, org.xml.sax.ContentHandler)}.
 	 * 
 	 * @throws IOException
 	 * @throws JSONParseException
@@ -32,8 +33,8 @@ public class JSON2SAXTest {
 		InputStream is = getClass().getResourceAsStream("EKSimExecuteResponse.json"); //$NON-NLS-1$
 		SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 		Schema schema = factory.newSchema(getClass().getResource("/ch/claninfo/pkone/pkOne-Meldungen.xsd")); //$NON-NLS-1$
-		ValidatorHandler validator = schema.newValidatorHandler();
-		JSON2SAX.parse(new InputStreamReader(is), "EKSimExecuteResponse", validator); //$NON-NLS-1$
+		Validator validator = schema.newValidator();
+		validator.validate(new DOMSource(JSON2DOM.parse(new InputStreamReader(is), "EKSimExecuteResponse"))); //$NON-NLS-1$
 	}
 
 }
